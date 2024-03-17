51c091a669a9e34c27692956f01fd0b8ecb47862
SultanovMuratB <137820590+SultanovMuratB@users.noreply.github.com>
2024-03-17 21:13:33 +0300

    first commit

---
+++ app/src/main/java/ru/sumin/servicestest/MyService.kt
@@ -0,0 +1,43 @@
+package ru.sumin.servicestest
+
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.os.IBinder
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class MyService : Service() {
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        scope.launch {
+            for (i in 0 until 100) {
+                delay(1000)
+            }
+        }
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+
+        fun newIntent(context: Context) : Intent {
+            return Intent(context, MyService::class.java)
+        }
+    }
+}
